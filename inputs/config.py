a061d7f4ad602c5da5fa9673a3e33c54ede859af
MsLolita <oblikovuizapuc@gmail.com>
2023-09-25 15:28:42 +0300

    upd recaptcha key

--- inputs/config.py
+++ inputs/config.py
@@ -1,6 +1,6 @@
 REFERRAL = "https://preregister.hytopia.com/kolya0blyat0rozban0v0chate"
 THREADS = 1  # Enter amount of threads
-CUSTOM_DELAY = (0, 0)  # (from 1 seconds, to 3 seconds), delay before every registration in seconds
+CUSTOM_DELAY = (0, 2)  # (from 1 seconds, to 3 seconds), delay before every registration in seconds
 
 # left empty if you use static proxies from file proxies.txt
 MOBILE_PROXY = ""  # login:password@ip:port - login1:a_password@2.56.119.93:5072
@@ -17,7 +17,7 @@ CAPTCHAAI_API_KEY = ""
 CAPTCHA_PARAMS = {
     "captcha_type": "v2",
     "invisible_captcha": True,
-    "sitekey": "6LccsxkoAAAAAM8JaxrLyR0uh9zz4mYE5Y00hR-5",
+    "sitekey": "6Leqr00oAAAAAN3ItHtrGkMpHiOtENMkG87lq2fq",
     "captcha_url": "https://preregister.hytopia.com/"
 }
 
